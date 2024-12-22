ff6a69503d58ca1ec7f89a10c09bb1cb3042b991
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2024-12-22 16:46:24 +0900

    Add email

--- packages/domain/src/user.ts
+++ packages/domain/src/user.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const userSchema = z.object({
 	id: z.string().brand<"UserId">(),
 	name: z.string().nonempty().max(10),
+	email: z.string().email(),
 });
 
 export type User = z.infer<typeof userSchema>;
