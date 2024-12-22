54c7152902be782f5d544cf5448a04acfe7adb5a
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2024-12-22 11:34:06 +0900

    Add user API

--- packages/core/src/api/user.ts
+++ packages/core/src/api/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import type { z } from "zod";
 import { type User, type UserId, userSchema } from "@project/domain";
 import { zValidator } from "@hono/zod-validator";
+import { nanoid } from "nanoid";
 
 const userCreateRequestSchema = userSchema.omit({ id: true });
 export type UserCreateRequest = z.infer<typeof userCreateRequestSchema>;
@@ -12,5 +13,22 @@ const userRepository: Map<UserId, User> = new Map();
 export const route = new Hono().post(
 	"/",
 	zValidator("json", userCreateRequestSchema),
-	async (req, res) => {},
-);
+	async (c) => {
+    const body = await c.req.json();
+    const newUser = userCreateRequestSchema.parse(body);
+    const id = nanoid(10) as UserId;
+    const user: User = { ...newUser, id };
+    userRepository.set(id, user);
+    return c.json(user);
+  }
+).get("/:id", async (c) => {
+  const id = c.req.param("id") as UserId;
+  const user = userRepository.get(id);
+  if (!user) {
+    c.status(404)
+    return c.json({ error: "User not found" });
+  }
+  return c.json(user);
+}).get("/", async (c) => {
+  return c.json([...userRepository.values()]);
+});
